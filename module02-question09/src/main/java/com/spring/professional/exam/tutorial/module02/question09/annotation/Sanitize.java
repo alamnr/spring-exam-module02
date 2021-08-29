f9b86163608bb57b6fa06de574628ac54295c093
alamnr <alamnr@gmail.com>
2021-08-29 20:53:30 +0600

    What is a ProceedingJoinPoint? When is it used?

---
+++ module02-question09/src/main/java/com/spring/professional/exam/tutorial/module02/question09/annotation/Sanitize.java
@@ -0,0 +1,10 @@
+package com.spring.professional.exam.tutorial.module02.question09.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+@Retention(RUNTIME)
+public @interface Sanitize {
+
+}
---
+++ module02-question09/src/test/java/com/spring/professional/exam/tutorial/module02/question09/ApplicationConfigurationTest.java
@@ -0,0 +1,20 @@
+package com.spring.professional.exam.tutorial.module02.question09;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationConfigurationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
